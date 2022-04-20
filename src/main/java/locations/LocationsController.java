0b3e19be269ec2b4e75db520a97434237c53abf6
mezeiberci95 <bertalan.mezei95@gmail.com>
2022-04-20 15:34:10 +0200

    201 CREATED status on POST, 404 on every IllegalArgumentException, 204 on DELETE

--- src/main/java/locations/LocationsController.java
+++ src/main/java/locations/LocationsController.java
@@ -1,7 +1,13 @@
 package locations;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,12 +26,19 @@ public class LocationsController {
         return locationsService.getLocations(name);
     }
 
+    // így is lehet állítani, hogy milyen státusszal menjen vissza, de van erre szabvány !!
     @GetMapping("/{id}")
-    public LocationDto findLocationById(@PathVariable("id") long id) {
-        return locationsService.findLocationById(id);
+    public ResponseEntity findLocationById(@PathVariable("id") long id) {
+        try {
+            return ResponseEntity.ok(locationsService.findLocationById(id));
+        } catch (IllegalArgumentException iae) {
+            return ResponseEntity.notFound().build();
+
+        }
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public LocationDto createLocation(@RequestBody CreateLocationCommand command) {
         return locationsService.createLocation(command);
     }
@@ -36,7 +49,25 @@ public class LocationsController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteLocation(@PathVariable("id") long id) {
         locationsService.deleteLocation(id);
+    }
+
+    // Ha valamelyik metódus IllegalArgumentException-t dob, akkor ez a rész fut le és állítja a Problem Details szabvány szerint a mezőket
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Problem> handleNotFound(IllegalArgumentException iae) {
+        Problem problem =
+                Problem.builder()
+                        .withType(URI.create("locations/not-found"))
+                        .withTitle("Not found")
+                        .withStatus(Status.NOT_FOUND)
+                        .withDetail(iae.getMessage())
+                        .build();
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_PROBLEM_JSON)
+                .body(problem);
     }
 }
--- src/main/java/locations/LocationsSpringApplication.java
+++ src/main/java/locations/LocationsSpringApplication.java
@@ -1,5 +1,6 @@
 package locations;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +16,13 @@ public class LocationsSpringApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	// ezzel lesz megfelelő JSON formátumban kiírva a Problem példány
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper()
+				.findAndRegisterModules();
 	}
 
 }
